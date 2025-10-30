a8032c26b99a6adb459e0836caeb7c3a34d88a41
chenxinquan <chenxinquan@pjlab.org.cn>
2025-10-30 14:45:43 +0800

    add dockerfile

--- examples/run_8_trading_envs.sh
+++ examples/run_8_trading_envs.sh
@@ -1,8 +1,8 @@
 python examples/base_eval.py \
-  --env-config-yaml "/mnt/shared-storage-user/chenxinquan/ai_sandbox/env/tradinggym/trading_env_configs.yaml" \
+  --env-config-yaml "/mnt/shared-storage-user/chenxinquan/ai_sandbox/env/tradinggym/trading_env.yaml" \
   --max-workers 64 \
   --max-steps 1000 \
-  --visual-save-path "/mnt/shared-storage-user/chenxinquan/ai_sandbox/visualize/test1027" \
+  --visual-save-path "/mnt/shared-storage-user/chenxinquan/ai_sandbox/visualize/test1030" \
   --agent-api-key "EMPTY" \
   --agent-base-url "http://localhost:8001/v1" \
   --agent-model "Qwen3-30B-Instruct" \
