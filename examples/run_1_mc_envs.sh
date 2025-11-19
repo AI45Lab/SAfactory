0142ae2cd30455407550eeca90c78d1d9b463170
root <root@sz-rollout-1-lhvgz-85994-worker-0.leishanzhe.ailab-steai.svc.pjlab.local>
2025-11-19 21:48:35 +0800

    1.docker;2.service

--- examples/run_1_mc_envs.sh
+++ examples/run_1_mc_envs.sh
@@ -1,9 +1,9 @@
 python examples/base_eval.py \
-  --env-config-yaml "/fs-computility-new/nuclear/leishanzhe/repo/AIEvoBox/env/mc/mc_env.yaml" \
+  --env-config-yaml "/mnt/shared-storage-user/leishanzhe/repo/AIEvoBox/env/mc/mc_env.yaml" \
   --max-workers 64 \
   --max-steps 1000 \
-  --visual-save-path "/fs-computility-new/nuclear/leishanzhe/repo/AIEvoBox/env/mc/visualize/test1107" \
+  --visual-save-path "/mnt/shared-storage-user/leishanzhe/repo/AIEvoBox/env/mc/visualize/test1107" \
   --agent-api-key "EMPTY" \
-  --agent-base-url "http://localhost:8000/v1" \
-  --agent-model "/fs-computility-new/nuclear/leishanzhe/shared/hf-hub/models--Qwen--Qwen2.5-VL-7B-Instruct/snapshots/cc594898137f460bfe9f0759e9844b3ce807cfb5" \
+  --agent-base-url "http://100.100.22.146:8000/v1" \
+  --agent-model "/mnt/shared-storage-user/evobox-share/hf-hub/models--Qwen2.5-VL-7B-Instruct" \
   --agent-temperature 0.3
