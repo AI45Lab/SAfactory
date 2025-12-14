baacd776a398ac835d71c6f6872736df10e180ef
root <root@gzk-evobox-c9l65-494800-worker-0.gaozhenkun.ailab-evobox.svc.pjlab.local>
2025-12-14 08:42:35 +0000

    fix

--- examples/run_8_embodied_envs1.sh
+++ examples/run_8_embodied_envs1.sh
@@ -4,6 +4,6 @@ python examples/base_eval.py \
   --max-steps 50 \
   --visual-save-path "/mnt/shared-storage-user/evobox-share/gaozhenkun/gzk/eval/visualize/test1111-1" \
   --agent-api-key "EMPTY" \
-  --agent-base-url "http://100.99.142.120:8001/v1" \
+  --agent-base-url "http://100.99.177.205:8001/v1" \
   --agent-model "/mnt/shared-storage-user/steai-share/hf-hub/Qwen2.5-VL-7B-Instruct" \
   --agent-temperature 0.3
