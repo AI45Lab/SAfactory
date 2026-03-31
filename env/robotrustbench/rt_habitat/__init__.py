ef346157a96f47b7cb04b5777f302ced53da3f64
huangchao <huangchao@pjlab.org.cn>
2026-03-31 12:23:07 +0000

    drop robotrustbench rt_habitat from git

--- env/robotrustbench/rt_habitat/__init__.py
+++
@@ -1,5 +0,0 @@
-﻿#
-# For licensing see accompanying LICENSE file.
-# Copyright (C) 2024 Apple Inc. All Rights Reserved.
-#
-# Keep package import side-effect free. Import submodules explicitly from callers.
--- env/robotrustbench/rt_habitat/dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-#
-# For licensing see accompanying LICENSE file.
-# Copyright (C) 2024 Apple Inc. All Rights Reserved.
-#
-from .episodes import LangRearrangeDatasetV0, LangRearrangeEpisode
